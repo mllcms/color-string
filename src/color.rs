ff168ef2079b9aa6033b2cce0cabeed8d55fab05
Mll <mllcms@163.com>
2023-11-03 05:00:01 +0800

    refactor: color_string 宏拆分为 cs and wcs

--- src/color.rs
+++ src/color.rs
@@ -1,9 +1,13 @@
-/// 快速生成彩色字体
+/// color_string 快速生成彩色字符串
 /// # Examples
 /// ```
-/// use color_string::color_string;
+/// use color_string::cs;
 /// use color_string::Font::*;
-/// let s = color_string! {
+/// let s = cs!(Red, Bold; "{:X}", u32::MAX);
+/// println!("{s}");
+/// assert_eq!("\u{1b}[0;31;1mFFFFFFFF\u{1b}[0m", s);
+///
+/// let s = cs!{
 ///     Red => 123456, "\n";
 ///     Green,Bold => "hello world";
 /// };
@@ -11,15 +15,48 @@
 /// assert_eq!("\u{1b}[0;31m123456\n\u{1b}[0;32;1mhello world\u{1b}[0m", s);
 /// ```
 #[macro_export]
-macro_rules! color_string {
-    ($( $($font:expr),* => $($s:expr),* );* $(;)?) => {{
-        use std::fmt::Write;
+macro_rules! cs {
+     ($($arg:tt)*) => {{
         let mut s = String::new();
-        $(
-            $crate::write_fonts!(&mut s, $($font),*);
-            $(write!(&mut s, "{}", $s).unwrap();)*
-        )*
-        s.push_str("\x1b[0m");
+        $crate::wcs!(&mut s, $($arg)*);
         s
+    }};
+}
+
+/// write_color_string 写入彩色字符串
+/// # Examples
+/// ```
+/// use color_string::wcs;
+/// use color_string::Font::*;
+/// let mut s = String::new();
+/// wcs!(&mut s, Red, Bold; "{:X}", u32::MAX);
+/// println!("{s}");
+/// assert_eq!("\u{1b}[0;31;1mFFFFFFFF\u{1b}[0m", s);
+///
+/// let mut s = String::new();
+/// wcs!{
+///     &mut s,
+///     Red => 123456, "\n";
+///     Green,Bold => "hello world";
+/// }
+/// println!("{s}");
+/// assert_eq!("\u{1b}[0;31m123456\n\u{1b}[0;32;1mhello world\u{1b}[0m", s);
+/// ```
+#[macro_export]
+macro_rules! wcs {
+     ($buf:expr, $($font:expr),* ; $($arg:tt)*) => {{
+        use std::fmt::Write;
+        $crate::write_fonts!($buf, $($font),*);
+        write!($buf, $($arg)*).unwrap();
+        $buf.push_str("\x1b[0m");
+    }};
+
+    ($buf:expr, $($($font:expr),* => $($s:expr),* );* $(;)?) => {{
+        use std::fmt::Write;
+        $(
+            $crate::write_fonts!($buf, $($font),*);
+            $(write!($buf, "{}", $s).unwrap();)*
+        )*
+        $buf.push_str("\x1b[0m");
     }};
 }
